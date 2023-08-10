3af108f52066bf8be701f08d0e061b926a2de309
r.ehdals <57896501+do4ng@users.noreply.github.com>
2023-08-10 10:48:17 +0900

    chore(build): replace esbuild to asto

--- scripts/build.js
+++ scripts/build.js
@@ -1,16 +1,14 @@
-const { asto, esbuildLoader } = require('asto');
-const { default: nodeExternalsPlugin } = require('esbuild-node-externals');
+/* eslint-disable function-paren-newline */
+const { asto } = require('asto');
+const { webpackLoader } = require('@asto/webpack');
+const webpack = require('webpack');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
 const repo = (name) => (input, output) => ({
   input: `packages/${name}/src/${input}`,
   output: `packages/${name}/dist/${output}`,
   options: {
-    plugins: [
-      nodeExternalsPlugin({
-        packagePath: `packages/${name}/package.json`,
-        devDependencies: true,
-      }),
-    ],
+    plugins: [],
   },
 });
 
@@ -19,19 +17,52 @@ const repos = {
   vitePlugin: repo('vite-plugin-zely'),
 };
 
-asto({
-  loader: esbuildLoader({
-    define: {
-      __ESM__: 'false',
-    },
+const webpackPlugins = [
+  new webpack.DefinePlugin({
+    __ESM__: false,
   }),
+];
+
+const loader = (cli = false) =>
+  webpackLoader(
+    {
+      typescript: true,
+      nodeExternals: true,
+      tsloader: {
+        transpileOnly: true,
+      },
+      cli,
+      stats(stats) {
+        if (stats.compilation.errors.length !== 0) {
+          console.log(stats.compilation.errors);
+        }
+      },
+    },
+    {
+      mode: 'production',
+      plugins: webpackPlugins,
+
+      resolve: {
+        plugins: [new TsconfigPathsPlugin.TsconfigPathsPlugin({})],
+      },
+    }
+  );
+
+asto({
+  loader: loader(),
+
   entryPoints: [
-    repos.zely('index', 'index.js'),
-    repos.zely('bin/index.ts', 'bin.js'),
+    repos.zely('index.ts', 'index.js'),
+    // repos.zely('bin/index.ts', 'bin.js'),
     repos.zely('server.ts', 'server.js'),
     repos.zely('export-config.ts', 'config.js'),
     repos.zely('export-methods.ts', 'methods.js'),
 
     repos.vitePlugin('index.ts', 'index.js'),
   ],
-});
+}).then(() =>
+  asto({
+    loader: loader(true),
+    entryPoints: [repos.zely('bin/index.ts', 'bin.js')],
+  })
+);
