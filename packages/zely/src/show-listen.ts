f7191895bf54894e2e715ad5d605f4b4dec0e93a
r.ehdals <57896501+do4ng@users.noreply.github.com>
2023-09-03 19:54:39 +0900

    feat: show zept version

--- packages/zely/src/show-listen.ts
+++ packages/zely/src/show-listen.ts
@@ -1,10 +1,14 @@
 import { box } from 'teeti';
+import loadModule from '$zely/lib/webpack';
 import 'colors';
 
 import pkg from '../package.json';
 
 export function showListen(port: number | string) {
-  console.log(`${'$'.gray} ${'zely'.yellow}${`v${pkg.version}`.yellow.dim}`);
+  console.log(
+    `${'-'.gray} ${`zept@${loadModule('zept/package.json').version}`.yellow.dim}`
+  );
+  console.log(`${'$'.gray} ${`zely@${pkg.version}`.yellow}`);
 
   console.log();
   const result = box(
