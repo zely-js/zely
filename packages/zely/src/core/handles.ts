b64f5e6c96a178c3f31c624bf00769a400321b1c
r.ehdals <57896501+do4ng@users.noreply.github.com>
2023-11-29 20:52:12 +0900

    refactor: split code

--- packages/zely/src/core/handles.ts
+++ packages/zely/src/core/handles.ts
@@ -3,110 +3,21 @@
 import url from 'node:url';
 import { IncomingMessage, ServerResponse } from 'node:http';
 import { existsSync, readFileSync } from 'node:fs';
-import { dirname, join, relative } from 'node:path';
-
-import { SourceMapConsumer } from 'source-map';
+import { join } from 'node:path';
 
 import { ObjectkeysMap } from '$zely/lib/chageKeys';
 import { pathToRegexp } from '$zely/lib/pathToRegexp';
-import { ServerDataHandler, ZelyRequest, ZelyResponse } from '$zely/types';
+import { ServerDataHandler, ZelyRequest, ZelyResponse } from '$zely';
 
 import { CACHE_DIRECTORY } from '../constants';
-import { error, errorWithStacks, parseError } from '../logger';
 import { Config } from '../config';
 import { Context } from '../server/context';
+import { errorHandler } from './error';
 
-const errorHandler = async (e) => {
-  if (!e) return;
-  if (process.env.NODE_ENV === 'production') {
-    throw new Error(e);
-  }
-
-  const stacks = parseError(e);
-  const occured = stacks[0].loc.slice(1, -1);
-  const sliced = occured.split(':');
-
-  const column = sliced.pop();
-  const line = sliced.pop();
-
-  const trace = {
-    filename: occured,
-    line: Number(line),
-    column: Number(column),
-  };
-  const tracer = JSON.parse(readFileSync(join(CACHE_DIRECTORY, 'tracer'), 'utf-8'));
-
-  if (!tracer[`${sliced.join(':').replace(/\\/g, '/')}.map`]) {
-    errorWithStacks(e.message, stacks);
-    return;
-  }
-
-  const sourcemap = new SourceMapConsumer(
-    JSON.parse(tracer[`${sliced.join(':').replace(/\\/g, '/')}.map`])
-  );
-
-  const result = (await sourcemap).originalPositionFor(trace);
-  const target = join(dirname(sliced.join(':')), result.source);
-
-  stacks.unshift({
-    at: '',
-    loc: `${target}:${result.line}:${result.column}`,
-  });
-
-  errorWithStacks(e.message, stacks);
-
-  const errorFile = readFileSync(target, 'utf-8').split('\n');
-  const errorLine = errorFile[result.line - 1];
-
-  console.log(
-    `${relative(process.cwd(), target)}:${result.line}:${result.column}`.yellow.dim
-  );
-
-  if (result.line - 1 > 0) {
-    console.log(`${`${result.line - 1} | `.gray}${errorFile[result.line - 2]}`);
-  }
-  console.log(
-    `${`${result.line} | `.gray}${errorLine.slice(0, result.column - 2)}${
-      errorLine.slice(result.column - 2, result.column + 2).underline.red
-    }${errorLine.slice(result.column + +2)}`
-  );
-  if (errorFile[result.line]) {
-    console.log(`${`${result.line + 1} | `.gray}${errorFile[result.line]}`);
-  }
-};
-
-export async function send(value: any, res: ZelyResponse) {
+export async function send(chunk: any, res: ZelyResponse) {
   if (res.writableEnded) return;
 
-  switch (typeof value) {
-    case 'string':
-      await res.send(value);
-      break;
-
-    case 'number':
-      await res.send((value as number).toString());
-      break;
-
-    case 'bigint':
-      await res.send((value as bigint).toString());
-      break;
-
-    case 'boolean':
-      await res.send(String(value));
-      break;
-
-    case 'undefined':
-      await res.send('undefined');
-      break;
-
-    case 'object':
-      await res.send(JSON.stringify(value));
-      break;
-
-    default:
-      error(`unsupported type: ${typeof value}`);
-      break;
-  }
+  res.send(chunk);
 }
 
 export async function handles(
