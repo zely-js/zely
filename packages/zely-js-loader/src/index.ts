758a1c94e73d5244c90c7accc9032e6ceec75362
r.ehdals <57896501+do4ng@users.noreply.github.com>
2024-01-10 20:56:58 +0900

    chore(@zely-js/loader): initial commit

---
+++ packages/zely-js-loader/src/index.ts
@@ -0,0 +1,38 @@
+/* eslint-disable no-unused-vars */
+import { esbuildLoader } from '@zely-js/loader-esbuild';
+
+import { relative } from 'node:path';
+import { readFileSync } from 'node:fs';
+
+import type { UserConfig } from '~/zely-js-core';
+import type { TransformOptions, TransformOutput } from '~/zely-js-loader';
+
+async function load(id: string) {
+  const relativePath = relative(__dirname, id).replace(/\\/g, '/');
+  if (__ESM__) {
+    return await import(relativePath);
+  }
+  return require(relativePath);
+}
+
+export function createLoader<T>(
+  options: UserConfig
+): (id: string, options?: TransformOptions<T>) => Promise<TransformOutput> {
+  if (!options.loader) {
+    options.loader = esbuildLoader(options);
+  }
+
+  return async (id, buildOptions) => {
+    const output = await options.loader?.transform(
+      id,
+      readFileSync(id).toString(),
+      buildOptions || { type: 'cache', buildOptions: {} }
+    );
+
+    return {
+      module: await load(output.filename),
+      filename: output.filename,
+      map: output.map,
+    };
+  };
+}
---
+++ packages/zely-js-loader/types/index.d.ts
@@ -0,0 +1,36 @@
+import { UserConfig } from '@zely-js/core';
+
+export interface TransformOutput {
+  /**
+   * output file path
+   */
+  filename: string;
+
+  /*
+   * out map file path
+   */
+  map: string;
+}
+
+export interface TransformOptions<T = any> {
+  type: 'page' | 'middleware' | 'configuration' | 'cache';
+  buildOptions: T;
+}
+
+export interface LoaderFunc extends TransformOutput {
+  module: any;
+}
+
+export interface Loader<T = any> {
+  name: string;
+
+  transform(
+    id: string,
+    source: string,
+    options: TransformOptions<T>
+  ): Promise<TransformOutput>;
+}
+
+export function createLoader<T = any>(
+  options: UserConfig
+): (id: string, options?: TransformOptions<T>) => Promise<LoaderFunc>;
