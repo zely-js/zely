cb0b94e17ce98b3c485d99808639fc2cdbaf0253
r.ehdals <57896501+do4ng@users.noreply.github.com>
2024-01-11 11:38:24 +0900

    chore: rename function

--- packages/zely-js-core/src/index.ts
+++ packages/zely-js-core/src/index.ts
@@ -1,4 +1,4 @@
-export { createZely } from './server';
+export { createZelyServer } from './server';
 export { controll } from './controller';
 export { methods } from './controller/methods';
 export * from './methods';
