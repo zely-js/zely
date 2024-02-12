16066a818532a7f6882fe18d78cf6ae552c0b20b
r.ehdals <57896501+do4ng@users.noreply.github.com>
2024-02-12 23:12:44 +0900

    chore: exclude typescript code

--- packages/zely-js/src/index.ts
+++ packages/zely-js/src/index.ts
@@ -2,3 +2,4 @@ export * from '@zely-js/core';
 export * from './plugin';
 export * from './server';
 export * from './config';
+export * from './prewrite';
--- packages/zely-js/types/index.d.ts
+++ packages/zely-js/types/index.d.ts
@@ -1,4 +1,4 @@
-import { createZelyServer } from '@zely-js/core';
+import { ZelyResponse, createZelyServer } from '@zely-js/core';
 import { Config } from './config';
 
 export type Server = Awaited<ReturnType<typeof createZelyServer>>['server'];
@@ -6,3 +6,7 @@ export function zely(options: Config): Promise<Server>;
 export * from '@zely-js/core';
 export { Config };
 export function defineConfig(config: Config): Config;
+export function usePrewrite(
+  res: ZelyResponse,
+  callback: (data: string) => Promise<string>
+): void;
