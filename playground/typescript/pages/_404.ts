a044c8133410f8d09d6a57468ce84fccebe5fc4e
r.ehdals <57896501+do4ng@users.noreply.github.com>
2024-02-12 21:23:39 +0900

    chore(playground): new playground

--- playground/typescript/pages/_404.ts
+++ playground/typescript/pages/_404.ts
@@ -2,6 +2,6 @@ import { GET } from '@zely-js/core';
 
 export default [
   GET((ctx) => {
-    ctx.status(404).send({ error: 'Page Not Found', code: 404 });
+    ctx.status(404).send({ error: 'Page Not Found!', code: 404 });
   }),
 ];
