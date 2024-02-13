ab6b87cdafbb6e558b637847a4b0734edd5830e1
r.ehdals <57896501+do4ng@users.noreply.github.com>
2024-02-13 09:56:12 +0900

    fix(type): fixed typescript error

--- packages/zely-js-core/types/middleware.d.ts
+++ packages/zely-js-core/types/middleware.d.ts
@@ -1,4 +1,8 @@
 import { Request, Response } from '@zept/http';
 // import { Context } from './server';
 
-export type Middleware = (req: Request, res: Response, next: () => void) => Promise<void>;
+export type Middleware = (
+  req: Request,
+  res: Response,
+  next: () => void
+) => Promise<void> | void;
