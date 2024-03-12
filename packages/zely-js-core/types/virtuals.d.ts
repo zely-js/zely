ee0bfd238927e575b4b55313e7e2ffc87d5093fd
r.ehdals <75308053+werewrwere@users.noreply.github.com>
2024-03-12 22:48:07 +0900

    refactor: add dts comments

--- packages/zely-js-core/types/virtuals.d.ts
+++ packages/zely-js-core/types/virtuals.d.ts
@@ -7,4 +7,9 @@ export type PageModule =
   | SupportEsm<PageHandlerResponse[]>
   | SupportEsm<Record<string, (ctx: Context) => any | Promise<any>>>;
 
+/**
+ * https://zely.vercel.app/apis/virtual-page
+ * @param filename virtual page filename (ex: index.ts)
+ * @param pageModule module
+ */
 export function createVirtualPage(filename: string, pageModule: PageModule): any;
