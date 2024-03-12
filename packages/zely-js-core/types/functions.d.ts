ca982549c9236b9ae1f8bf30a8488452c738c297
r.ehdals <75308053+werewrwere@users.noreply.github.com>
2024-03-12 23:01:01 +0900

    refactor: add dts comments

--- packages/zely-js-core/types/functions.d.ts
+++ packages/zely-js-core/types/functions.d.ts
@@ -1,6 +1,15 @@
 import { ZeptServer } from '@zept/http';
 import { UserConfig } from './config';
 
+/**
+ * create server instance
+ *
+ * ```ts
+ * const { server, applyZelyMiddlewares } = await createZelyServer({});
+ * applyZelyMiddlewares(server);
+ * ```
+ * @param options Config
+ */
 export function createZelyServer(options: UserConfig): Promise<{
   server: ZeptServer;
   applyZelyMiddlewares: (serverInstance: ZeptServer) => void;
