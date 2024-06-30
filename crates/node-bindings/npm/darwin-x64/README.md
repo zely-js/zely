4bc87f2c7c2ecfb286c257d63913a7b4f5e2ef35
r.ehdals <57896501+do4ng@users.noreply.github.com>
2024-06-30 22:44:39 +0900

    chore(rust): remove rust (#315)

--- crates/node-bindings/npm/darwin-x64/README.md
+++
@@ -1,3 +0,0 @@
-# `zely-darwin-x64`
-
-This is the **x86_64-apple-darwin** binary for `zely`
--- crates/node-bindings/src/lib.rs
+++
@@ -1,9 +0,0 @@
-#![deny(clippy::all)]
- 
-#[macro_use]
-extern crate napi_derive;
-
-#[napi]
-fn sum(a: u32, b: u32) -> u32 {
-	a + b
-}
