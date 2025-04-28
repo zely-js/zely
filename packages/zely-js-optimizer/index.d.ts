102b63c6c9d3b0da48226bd84b3a4d3303d93135
r.ehdals <kangdongmandoo@gmail.com>
2025-04-28 19:45:33 +0900

    chore(types): export cache function

--- packages/zely-js-optimizer/index.d.ts
+++ packages/zely-js-optimizer/index.d.ts
@@ -1,5 +1,7 @@
+import type { Context } from '@zely-js/core';
 import type { CompilerOptions, Plugin } from 'serpack';
 
 export interface OptimizerOptions {}
 
 export function optimizer(options?: OptimizerOptions): Plugin;
+export function $serpack_cache(value: any, context: Context, name: string): Promise<any>;
