e888cdd09180bb7672bf892805dac81e51ff801d
r.ehdals <kangdongmandoo@gmail.com>
2025-05-04 21:29:35 +0900

    feat: add some files to template

---
+++ packages/zely-js-cli/assets/javascript/pages/[name].js
@@ -0,0 +1,16 @@
+// This file is an example of caching server data (powered by @zely-js/optimizer).
+// See more: https://zely.vercel.app/docs/server-data
+
+import { setTimeout } from 'timers/promises';
+
+async function $greeting(name) {
+  await setTimeout(1000);
+  return `Hello, ${name}!`;
+}
+
+module.exports = [
+  GET(async (ctx) => {
+    const message = await $greeting(ctx.params.name);
+    ctx.send(message);
+  }),
+];
