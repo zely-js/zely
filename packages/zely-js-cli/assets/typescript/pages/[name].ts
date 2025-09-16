3decd0d7ed5c254f2f353b9f61eca78236405672
r.ehdals <kangdongmandoo@gmail.com>
2025-09-16 23:26:18 +0900

    chore: update template for caching

--- packages/zely-js-cli/assets/typescript/pages/[name].ts
+++ packages/zely-js-cli/assets/typescript/pages/[name].ts
@@ -1,22 +1,19 @@
 export default [
   GET(async (ctx) => {
     // Use $store to cache the result of a time-consuming function
-    // https://zely.vercel.app/docs/server-data
-    const { data } = await $store(
-      async () => {
-        const host = ctx.request.headers.host ?? 'localhost:3000';
-        const url = `http://${host}/static/static.txt`;
+    // https://zely.vercel.app/docs/caching
+    const { data } = await $store(async () => {
+      const host = ctx.request.headers.host ?? 'localhost:3000';
+      const url = `http://${host}/static/static.txt`;
 
-        // If your node version is lower than 18,
-        // download a fetch package such as node-fetch and run this code.
-        const response = await fetch(url);
-        const text = await response.text();
+      // If your node version is lower than 18,
+      // download a fetch package such as node-fetch and run this code.
+      const response = await fetch(url);
+      const text = await response.text();
 
-        return text;
-      },
-      // Cache is separated by different values of ctx.params.name
-      [ctx.params.name]
-    );
+      return text;
+    }, ctx);
+    // Cache is separated by different values of ctx.params
 
     ctx.send(`${data}, ${ctx.params.name}`);
   }),
